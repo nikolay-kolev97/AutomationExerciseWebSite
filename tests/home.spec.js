78df9db07b8412043661585df517472bd3966cab
Nikolay Kolev <nikitospeeder@abv.bg>
2026-09-09 16:58:32 +0300

    add correction code

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 
-test.only("Open web site", async({page})=> {
+test("Open web site", async({page})=> {
     await page.goto('/');
     await page.getByRole('button', {name: 'Consent'}).click();
     await page.goto('/products');
